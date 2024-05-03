95380e6e3be814fdc492b8cfecdc3a8c13f8f52e
Ryan <ryancdale92@gmail.com>
2024-05-03 11:20:18 -0400

    Update constants.py

--- openbb_platform/core/openbb_core/app/constants.py
+++ openbb_platform/core/openbb_core/app/constants.py
@@ -1,6 +1,15 @@
 """Constants for the OpenBB Platform."""
 
+import os
 from pathlib import Path
+
+try:
+    home_path = os.environ["OPENBB_USER_DATA_DIRECTORY"]
+except KeyError:
+    home_path = Path.home()
+
+# Testing
+home_path = '/tmp'
 
 HOME_DIRECTORY = Path.home()
 OPENBB_DIRECTORY = Path(HOME_DIRECTORY, ".openbb_platform")
